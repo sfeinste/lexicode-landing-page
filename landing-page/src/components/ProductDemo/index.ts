80e101e881e09dc4ab033fb807af6e05530dbc54
Spruce Feinstein <sprucefeinstein@gmail.com>
2025-07-24 20:13:23 -0500

    removing landing page

--- landing-page/src/components/ProductDemo/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as BrowserWindow } from './BrowserWindow';
-export { default as CodeBlock } from './CodeBlock';
-export { default as DocumentationSection } from './DocumentationSection';
-export { default as DashboardMetrics } from './DashboardMetrics';
-export { default as DocumentationHealth } from './DocumentationHealth';
-export { default as ActivityInsights } from './ActivityInsights';
-export { default as ActivityFeed } from './ActivityFeed';
-export { default as ProgressBar } from './ProgressBar';
